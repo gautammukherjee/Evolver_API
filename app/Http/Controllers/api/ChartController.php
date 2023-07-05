79336dfacf88845322e969042c8eb9df1c92616d
piyushsrivastava80 <psrivastava@bioxcel.com>
2023-07-05 13:05:35 +0530

    backend API

--- app/Http/Controllers/api/ChartController.php
+++ app/Http/Controllers/api/ChartController.php
@@ -8,39 +8,69 @@ use Illuminate\Support\Facades\DB;
 
 class ChartController extends Controller
 {
-    public function distributionByRelationGrp(Request $request){
-
-        //echo "edge_select:".$request->input('edge_select');
-        list($day,$month,$year) = explode("-",$request->input('from_date'));
-        $from_date = $year.'-'.$month.'-'.$day;
+    public function distributionByRelationGrp(Request $request)
+    {
+        // //echo "edge_select:".$request->input('edge_select');
+        // list($day, $month, $year) = explode("-", $request->input('from_date'));
+        // $from_date = $year . '-' . $month . '-' . $day;
         //echo $from_date;
 
-        $sql = "SELECT \"source\".\"Temp Edge Types_Name\" AS \"Temp Edge Types_Name\", COUNT(*) AS \"count\"
-        FROM (SELECT sl.\"pmid\" AS \"pmid\", sl.\"publication_date\" AS \"publication_date\", sl.\"title\" AS \"title\", neslr.\"pmid\" AS \"Node Edge Sci Lit Rels__pmid\",
-        nnrtn.\"name\" AS \"Node Node Relation Types\", 
-        nsn.\"name\" AS \"Source_Node_Name\", 
-        ndn.\"name\" AS \"Destination_Node_Name\", 
-        et.\"name\" AS \"Edge Types_Name\",
-        tet.\"group_name\" AS \"Temp Edge Types_Name\"
-        FROM \"source\".\"sci_lits\" as sl
-        INNER JOIN \"graphs\".\"node_edge_sci_lit_rels\" AS neslr ON sl.\"pmid\" = neslr.\"pmid\"
-        LEFT JOIN \"graphs\".\"node_edge_rels\" AS nern ON neslr.\"ne_id\" = nern.\"id\" 
-        LEFT JOIN \"graphs\".\"node_node_relation_types\" AS nnrtn ON nern.\"nnrt_id\" = nnrtn.\"nnrt_id\" 
-        LEFT JOIN \"graphs\".\"nodes\" AS nsn ON nern.\"source_node\" = nsn.\"node_id\" 
-        LEFT JOIN \"graphs\".\"node_edge_rels\" AS ner ON nern.\"id\" = ner.\"id\" 
-        LEFT JOIN \"graphs\".\"nodes\" AS ndn ON nern.\"destination_node\" = ndn.\"node_id\" 
-        LEFT JOIN \"graphs\".\"edge_types\" AS et ON nern.\"edge_type_id\" = et.\"edge_type_id\"
-        LEFT JOIN \"graphs\".\"temp_edge_type_group\" AS tet ON tet.\"edge_type_id\" = nern.\"edge_type_id\"
-        Where sl.publication_date > '2017-06-01'
-           AND nsn.name <> ndn.name AND nsn.name NOT IN ('WAS','IMPACT', 'HR', 'SIT') AND ndn.name NOT IN ('WAS','IMPACT', 'HR', 'SIT')
+        $sql = "SELECT source.edge_type_id, source.Temp_Edge_Types_Name AS Temp_Edge_Types_Name, COUNT(*) as count
+		FROM (SELECT sl.pmid AS pmid, sl.publication_date AS publication_date, sl.title AS title, 
+        neslr.pmid AS Node_Edge_Sci_Lit_Rels_pmid,
+        nnrtn.name AS Node_Node_Relation_Types, 
+        nnrtn.nnrt_id,	  
+        nsn.name AS Source_Node_Name,
+        nsn.node_id as source_node_id,	  
+        ndn.name AS Destination_Node_Name,
+        ndn.node_id as destination_node_id,	  
+        et.name AS Edge_Types_Name,
+	    tet.edge_type_id,
+        tet.group_name AS Temp_Edge_Types_Name FROM source.sci_lits as sl 
+        INNER JOIN graphs.node_edge_sci_lit_rels AS neslr ON sl.pmid = neslr.pmid
+        JOIN graphs.node_edge_rels AS nern ON neslr.ne_id = nern.id 
+        JOIN graphs.node_node_relation_types AS nnrtn ON nern.nnrt_id = nnrtn.nnrt_id 
+        JOIN graphs.nodes AS nsn ON nern.source_node = nsn.node_id 
+        JOIN graphs.node_edge_rels AS ner ON nern.id = ner.id 
+        JOIN graphs.nodes AS ndn ON nern.destination_node = ndn.node_id 
+        JOIN graphs.edge_types AS et ON nern.edge_type_id = et.edge_type_id
+        LEFT JOIN graphs.temp_edge_type_group AS tet ON tet.edge_type_id = nern.edge_type_id ";
 
+        // $sql = $sql . "Where sl.publication_date > '2017-06-01' AND";
 
-        ) AS \"source\"
-        WHERE (\"source\".\"Node Node Relation Types\" = 'Gene To Disease') AND (\"source\".\"Source_Node_Name\" = 'PDCD1') AND (\"source\".\"Destination_Node_Name\" = 'ADENOCARCINOMA')
-        GROUP BY \"source\".\"Temp Edge Types_Name\"
-        ORDER BY \"source\".\"Temp Edge Types_Name\" ASC";
+        $sql = $sql . " Where ";
+        $sql = $sql . " nsn.node_id <> ndn.node_id ";
+        // $sql = $sql . " AND nsn.name NOT IN ('WAS','IMPACT', 'HR', 'SIT') AND ndn.name NOT IN ('WAS','IMPACT', 'HR', 'SIT')";
 
-        //echo $sql;
+        $sourceNode = collect($request->source_node);
+        $sourceNodeImplode = $sourceNode->implode(', ');
+        // echo "heree2: " . $sourceNodeImplode;
+        if (!empty($sourceNodeImplode))
+            $sql = $sql . " AND nsn.node_id in (" . $sourceNodeImplode . ")"; // pass node-node relation type id
+        // }
+
+        //2. Destination Node
+        $destinationNode = collect($request->destination_node);
+        $destinationNodeImplode = $destinationNode->implode(', ');
+        // echo "heree2: " . $destinationNodeImplode;
+        if (!empty($destinationNodeImplode))
+            $sql = $sql . " AND ndn.node_id in (" . $destinationNodeImplode . ")"; // pass node-node relation type id
+
+        //3. Edge level 1
+        $edgeType = collect($request->edge_type_id);
+        $edgeTypeImplode = $edgeType->implode(', ');
+        // echo "heree3: " . $edgeTypeImplode;
+        if (!empty($edgeTypeImplode))
+            $sql = $sql . " AND nern.edge_type_id IN (" . $edgeTypeImplode . ")"; //pass edge_type_id for Level 1
+
+        //4. Node select level 1
+        if ($request->nnrt_id != "") {
+            $sql = $sql . " AND nern.nnrt_id = " . $request->nnrt_id; // pass node-node relation type id
+        }
+
+        $sql = $sql . " ) AS source";
+        $sql = $sql . " GROUP BY 1,2 ORDER BY 1 ASC";
+        // echo $sql;
         $result = DB::select($sql);
         return response()->json([
             'nodeSelectsRecords' => $result
@@ -49,33 +79,64 @@ class ChartController extends Controller
     } //distributionByRelationGrp() ends
 
 
-    public function details_of_association_type(Request $request){
-        $sql = "SELECT \"source\".\"Node Node Relation Types\" AS \"Node Node Relation Types\", COUNT(*) AS \"count\"
-        FROM (SELECT sl.\"pmid\" AS \"pmid\", sl.\"publication_date\" AS \"publication_date\", sl.\"title\" AS \"title\", neslr.\"pmid\" AS \"Node Edge Sci Lit Rels__pmid\",
-        nnrtn.\"name\" AS \"Node Node Relation Types\", 
-        nsn.\"name\" AS \"Source_Node_Name\", 
-        ndn.\"name\" AS \"Destination_Node_Name\", 
-        et.\"name\" AS \"Edge Types_Name\",
-        tet.\"group_name\" AS \"Temp Edge Types_Name\"
-        FROM \"source\".\"sci_lits\" as sl
-        INNER JOIN \"graphs\".\"node_edge_sci_lit_rels\" AS neslr ON sl.\"pmid\" = neslr.\"pmid\"
-        LEFT JOIN \"graphs\".\"node_edge_rels\" AS nern ON neslr.\"ne_id\" = nern.\"id\" 
-        LEFT JOIN \"graphs\".\"node_node_relation_types\" AS nnrtn ON nern.\"nnrt_id\" = nnrtn.\"nnrt_id\" 
-        LEFT JOIN \"graphs\".\"nodes\" AS nsn ON nern.\"source_node\" = nsn.\"node_id\" 
-        LEFT JOIN \"graphs\".\"node_edge_rels\" AS ner ON nern.\"id\" = ner.\"id\" 
-        LEFT JOIN \"graphs\".\"nodes\" AS ndn ON nern.\"destination_node\" = ndn.\"node_id\" 
-        LEFT JOIN \"graphs\".\"edge_types\" AS et ON nern.\"edge_type_id\" = et.\"edge_type_id\"
-        LEFT JOIN \"graphs\".\"temp_edge_type_group\" AS tet ON tet.\"edge_type_id\" = nern.\"edge_type_id\"
-        Where sl.publication_date > '2017-06-01'
-           AND nsn.name <> ndn.name AND nsn.name NOT IN ('WAS','IMPACT', 'HR', 'SIT') AND ndn.name NOT IN ('WAS','IMPACT', 'HR', 'SIT')
+    public function details_of_association_type(Request $request)
+    {
+        $sql = "SELECT source.nnrt_id,  
+        source.Node_Node_Relation_Types AS Node_Node_Relation_Types,
+        COUNT(*) AS count
+        FROM (SELECT sl.pmid AS pmid, sl.publication_date AS publication_date, sl.title AS title, 
+        neslr.pmid AS Node_Edge_Sci_Lit_Rels_pmid,
+        nnrtn.name AS Node_Node_Relation_Types, 
+        nnrtn.nnrt_id,	  
+        nsn.name AS Source_Node_Name,
+        nsn.node_id as source_node_id,	  
+        ndn.name AS Destination_Node_Name,
+        ndn.node_id as destination_node_id,	  
+        et.name AS Edge_Types_Name,
+        tet.group_name AS Temp_Edge_Types_Name FROM source.sci_lits as sl 
+        INNER JOIN graphs.node_edge_sci_lit_rels AS neslr ON sl.pmid = neslr.pmid
+        JOIN graphs.node_edge_rels AS nern ON neslr.ne_id = nern.id 
+        JOIN graphs.node_node_relation_types AS nnrtn ON nern.nnrt_id = nnrtn.nnrt_id 
+        JOIN graphs.nodes AS nsn ON nern.source_node = nsn.node_id 
+        JOIN graphs.node_edge_rels AS ner ON nern.id = ner.id 
+        JOIN graphs.nodes AS ndn ON nern.destination_node = ndn.node_id 
+        JOIN graphs.edge_types AS et ON nern.edge_type_id = et.edge_type_id
+        LEFT JOIN graphs.temp_edge_type_group AS tet ON tet.edge_type_id = nern.edge_type_id ";
 
+        $sql = $sql . " Where ";
+        // -- sl.publication_date > '2017-06-01 and ' 
+        $sql = $sql . " nsn.node_id <> ndn.node_id";
+        // $sql = $sql . " AND nsn.name NOT IN ('WAS','IMPACT', 'HR', 'SIT') AND ndn.name NOT IN ('WAS','IMPACT', 'HR', 'SIT')";
 
-        ) AS \"source\"
-        WHERE \"source\".\"Source_Node_Name\" = 'ADA'
-        GROUP BY \"source\".\"Node Node Relation Types\"
-        ORDER BY \"source\".\"Node Node Relation Types\" ASC";
+        $sourceNode = collect($request->source_node);
+        $sourceNodeImplode = $sourceNode->implode(', ');
+        // echo "heree2: " . $sourceNodeImplode;
+        if (!empty($sourceNodeImplode))
+            $sql = $sql . " AND nsn.node_id in (" . $sourceNodeImplode . ")"; // pass node-node relation type id
+        // }
 
-        //echo $sql;
+        //2. Destination Node
+        $destinationNode = collect($request->destination_node);
+        $destinationNodeImplode = $destinationNode->implode(', ');
+        // echo "heree2: " . $destinationNodeImplode;
+        if (!empty($destinationNodeImplode))
+            $sql = $sql . " AND ndn.node_id in (" . $destinationNodeImplode . ")"; // pass node-node relation type id
+
+        //3. Edge level 1
+        $edgeType = collect($request->edge_type_id);
+        $edgeTypeImplode = $edgeType->implode(', ');
+        // echo "heree3: " . $edgeTypeImplode;
+        if (!empty($edgeTypeImplode))
+            $sql = $sql . " AND nern.edge_type_id IN (" . $edgeTypeImplode . ")"; //pass edge_type_id for Level 1
+
+        //4. Node select level 1
+        if ($request->nnrt_id != "") {
+            $sql = $sql . " AND nern.nnrt_id = " . $request->nnrt_id; // pass node-node relation type id
+        }
+        $sql = $sql . " ) AS source";
+        $sql = $sql . " GROUP BY 1,2 ORDER BY 1 ASC";
+
+        // echo $sql;
         $result = DB::select($sql);
         return response()->json([
             'nodeSelectsRecords' => $result
